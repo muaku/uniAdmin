5f70fe66459867968f7e752b5d794d471fd9cd7a
muaku <m_kaojupor@yahoo.com>
2017-05-20 21:42:15 +0900

    second commit

--- questions/question.js
+++ questions/question.js
@@ -1,94 +1,94 @@
 var quizeModel = require("../models/model").quizeModel
 
 //create question
-exports.create = (req,res)=>{
-	var data = req.body
-	var questionModel = new quizeModel({
-		question:data.question,
-		category:data.category,
-		answers:data.answers,
-		credit:data.credit,
-		time:data.time
-	})
+exports.create = (req, res) => {
+    var data = req.body
+    var questionModel = new quizeModel({
+        question: data.question,
+        category: data.category,
+        answers: data.answers,
+        credit: data.credit,
+        time: data.time
+    })
 
-	quizeModel.findOne({
-		question:req.body.question
-	},function(err,data){
-		if(!data){
-			questionModel.save(function(err,success){
-				if(err){
-					console.log('Error got' + err)
-				}else{
-					console.log("Request got: " + success)
-					res.status(201).send()
-				}
-			});
-		}else{
-			console.log("Question exists")
-		}
-	})
+    console.log("data", data)
+
+    quizeModel.findOne({
+        question: req.body.question
+    }, function(err, data) {
+        if (!data) {
+            questionModel.save(function(err, success) {
+                if (err) {
+                    console.log('Error got' + err)
+                    res.status(500).send("Something Error")
+                } else {
+                    console.log("Request got: " + success)
+                    res.status(201).send("DONE")
+                }
+            });
+        } else {
+            console.log("Question exists")
+            res.status(200).send("Question exists")
+        }
+    })
 }
 
 //question update
-exports.edite=(req,res)=>{
-	let questionId = req.params.id
-	quizeModel.findById(questionId,function(err,data){
-		data.question = req.body.question;
-		data.category = req.body.category;
-		data.credit = req.body.credit
-		data.time = req.body.time
-		data.answers = req.body.answers
-		console.log(data)
-		data.save(function(err,data){
-			if(err){
-				res.status(500).send(err)
-			}
-			console.log('update:',data)
-			res.status(200).send()
-		})
+exports.edite = (req, res) => {
+    let questionId = req.params.id
+    quizeModel.findById(questionId, function(err, data) {
+        data.question = req.body.question;
+        data.category = req.body.category;
+        data.credit = req.body.credit
+        data.time = req.body.time
+        data.answers = req.body.answers
+        console.log(data)
+        data.save(function(err, data) {
+            if (err) {
+                res.status(500).send(err)
+            }
+            console.log('update:', data)
+            res.status(200).send()
+        })
 
-	})
+    })
 }
 
 //delete question
-exports.delete = (req,res)=>{
-	let questionId = req.params.id
-	console.log(questionId);
+exports.delete = (req, res) => {
+    let questionId = req.params.id
+    console.log(questionId);
 
-	quizeModel.findByIdAndRemove(questionId,function(err,data){
-		if(err){
-			console.log("got error from questionId")
-		}else{
-			console.log("deleted success question:",data)
-			res.status(204).send()
-		}
-	})
+    quizeModel.findByIdAndRemove(questionId, function(err, data) {
+        if (err) {
+            console.log("got error from questionId")
+        } else {
+            console.log("deleted success question:", data)
+            res.status(204).send()
+        }
+    })
 }
 
 //get question by category
-exports.get = (req,res)=>{
-	let qcategory = req.params.category
-	console.log(qcategory)
-	quizeModel.find({'category':qcategory},function(err,doc){
-		if(err) return next(err)
-		console.log(doc)
-		res.status(200).send(doc)
-	})
+exports.get = (req, res) => {
+    let qcategory = req.params.category
+    console.log(qcategory)
+    quizeModel.find({ 'category': qcategory }, function(err, doc) {
+        if (err) return next(err)
+        console.log(doc)
+        res.status(200).send(doc)
+    })
 }
 
 //get question by cateogory and Id
-exports.getLimit = (req,res)=>{
-	let category = req.params.category
-	let value = parseInt(req.params.limit)
-	console.log(typeof(value))
-	console.log(category)
-	console.log(value)
-	quizeModel.find({"category":category}).limit(value).exec(function(err, data){
-		console.log(data)
-		res.status(200).send(data)
-	})
+exports.getLimit = (req, res) => {
+    let category = req.params.category
+    let value = parseInt(req.params.limit)
+    console.log(typeof(value))
+    console.log(category)
+    console.log(value)
+    quizeModel.find({ "category": category }).limit(value).exec(function(err, data) {
+        console.log(data)
+        res.status(200).send(data)
+    })
 }
-
-
-
-
--- server.js
+++ server.js
@@ -5,30 +5,32 @@ var path = require('path')
 var session = require('express-session')
 var randomString = require('randomstring')
 var app = express()
+var helmet = require('helmet')
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/quizapp')
-var server = app.listen(3000,function(){
-	var host = server.address().address
-	var port = server.address().port
-	console.log("App listening at http://%s:%s",host,port)
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("App listening at http://%s:%s", host, port)
 })
 var sessionCheck = require('./middle_ware/middleware')
 var router = require('./routers/route.js')
 var login = require('./user/login.js')
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(cookieParser());
 //console.log(randomString.generate())
 app.use(session({
-	secret:randomString.generate(),
-	saveUninitialized:false,
-	resave:false,
-	cookie:{
-		maxAge:30*60*1000
-	}
+    secret: randomString.generate(),
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 30 * 60 * 1000
+    }
 }));
 console.log(session)
-app.use('/login',login);
-app.use('/',sessionCheck,router);
+app.use('/login', login);
+app.use('/', sessionCheck, router);
 //app.use('/',router)
